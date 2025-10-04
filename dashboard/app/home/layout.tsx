2f40664b6e502f1251d7181cbb5bf40bf9e97e19
Faruoq01 <59730816+Faruoq01@users.noreply.github.com>
2025-10-04 09:15:26 +0100

    Update layout.tsx

--- dashboard/app/home/layout.tsx
+++ dashboard/app/home/layout.tsx
@@ -5,6 +5,30 @@ import Sidebar from "../components/home/sidebar";
 import Header from "../components/home/header";
 
 const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.auth.user);
+  const isLoggedIn = useAppSelector((state) => state.auth.isLoggedIn);
+
+  const getAuthUser = useCallback(async () => {
+    const { error, payload } = await AuthService.getAuthUser();
+    if (!error && payload) {
+      dispatch(setUser(payload.data));
+    }
+  }, []);
+
+  useEffect(() => {
+    if (Object.values(user).length === 0) {
+      getAuthUser();
+    }
+  }, [getAuthUser]);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      router.push(AppPages.auth.login);
+    }
+  }, [isLoggedIn]);
+
   return (
     <div className="h-full w-full flex flex-row bg-gray-200">
       <Sidebar />
